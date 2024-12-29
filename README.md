b9a0baa5658179a463200f56b6216434ba5b9da9
hbhungg <hung.ba.huynh@proton.me>
2024-12-29 23:28:15 +1100

    can print board

---
+++ README.md
@@ -0,0 +1,5 @@
+# python-xiangqi: a xiangqi library for Python
+
+Like [python-chess](https://github.com/niklasf/python-chess), but for [xiangqi](https://en.wikipedia.org/wiki/Xiangqi).
+
+Project is still in early WIP.
--- engine.py
+++ engine.py
@@ -1,14 +1,10 @@
+from collections import namedtuple
+from enum import Enum
+from typing import Optional
+
+
 RANKS = 10
 FILES = 9
-
-def bitboard_to_string(bitboard: int):
-  ret = ""
-  for rank in range(RANKS-1, -1, -1):
-    row = ' '.join([f'{(bitboard >> (rank * FILES + file)) & 1}' for file in range(FILES-1, -1, -1)])
-    ret += f"{row}\n"
-  return ret
-
-
 STARTING_POSITION = {
   "RED_SOLDIER"    : 0b000000000_000000000_000000000_000000000_000000000_000000000_101010101_000000000_000000000_000000000,
   "RED_CANNON"     : 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_010000010_000000000_000000000,
@@ -27,17 +23,24 @@ STARTING_POSITION = {
   "BLACK_CHARIOT"  : 0b100000001_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000,
 }
 
+class Side(Enum):
+  RED=True
+  BLACK=False
+
 class Piece:
   VERTICAL = 0b100000000_100000000_100000000_100000000_100000000_100000000_100000000_100000000_100000000_100000000
   HORIZONTAL = 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_111111111
   NAME: str
 
-  def __init__(self, side: bool):
+  def __init__(self, side: Side):
     piece_name = self.__class__.NAME
-    self.bit = STARTING_POSITION[f"RED_{piece_name}"] if side else STARTING_POSITION[f"BLACK_{piece_name}"]
-
+    self.side = side
+    self.bit = STARTING_POSITION[f"RED_{piece_name}"] if side == Side.RED else STARTING_POSITION[f"BLACK_{piece_name}"]
+  
   def __repr__(self):
-    return bitboard_to_string(self.bit)
+    return self.NAME[0].upper() if self.side == Side.RED else self.NAME[0].lower()
+  # def __repr__(self):
+  #   return bitboard_to_string(self.bit)
 
 class Soldier(Piece):
   NAME = "SOLDIER"
@@ -57,18 +60,34 @@ class Chariot(Piece):
 class Board:
   def __init__(self):
     piece_types = [Soldier, Cannon, General, Advisor, Elephant, Horse, Chariot]
-    self.red_pieces: list[Piece] = [piece(side=True) for piece in piece_types]
-    self.black_pieces: list[Piece] = [piece(side=False) for piece in piece_types]
+    self.red_pieces: list[Piece] = [piece(side=Side.RED) for piece in piece_types]
+    self.black_pieces: list[Piece] = [piece(side=Side.BLACK) for piece in piece_types]
+    self.all_pieces = self.red_pieces + self.black_pieces
+    self.turn = Side.RED
   
-  def comb(self):
-    from functools import reduce
-    bit = reduce(lambda x, y: x | y, [i.bit for i in self.red_pieces + self.black_pieces])
-    return bit
+  @staticmethod
+  def parse_move(move: str):
+    r, c = move[0], move[1]
+    r = "ABCDEFGHI".index(r.upper())
+    c = int(c)
+    return r, c
+
+  def at(self, start: tuple[int, int]) -> Optional[Piece]:
+    mask = 1 << (start[0] * FILES + start[1])
+    for piece in self.all_pieces:
+      if piece.bit & mask:
+        return piece
+    return None
 
   def __repr__(self):
-    return bitboard_to_string(self.comb())
+    ret = ""
+    for i in range(RANKS):
+      row = " ".join(str(s) if (s := self.at(start=(i, j))) is not None else "." for j in range(FILES))
+      ret = f"{row}\n{ret}"
+    return ret
 
 if __name__ == "__main__":
   b = Board()
   print(b)
+ 
 
