5a8568abf7f833c03500fc980c3d6c5c25b62b41
hbhungg <hung.ba.huynh@proton.me>
2024-12-29 23:51:29 +1100

    simplest test I can think of

--- test/test_core.py
+++ test/test_core.py
@@ -1,5 +1,17 @@
 from xiangqi import Board
 
-def test_simple():
+def test_board_print():
+  starting_board = """
+c h e a g a e h c
+. . . . . . . . .
+. c . . . . . c .
+s . s . s . s . s
+. . . . . . . . .
+. . . . . . . . .
+S . S . S . S . S
+. C . . . . . C .
+. . . . . . . . .
+C H E A G A E H C
+"""
   b = Board()
-  print(b)
+  assert str(b).strip() == starting_board.strip()
