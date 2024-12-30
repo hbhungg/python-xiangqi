059fdd5980e57d6bf26139633efc559e281995c8
hbhungg <hung.ba.huynh@proton.me>
2024-12-30 12:48:22 +1100

    massive cleanup

--- xiangqi/__init__.py
+++ xiangqi/__init__.py
@@ -1,5 +1,6 @@
 from enum import Enum, auto
 from typing import Optional
+from dataclasses import dataclass, field
 
 
 RANKS = 10
@@ -19,7 +20,7 @@ class PieceType(Enum):
 
 
 def piece_symbol(piece_type: PieceType) -> str:
-  return piece_type.name[0].lower()
+  return piece_type.name[0].lower() if piece_type != PieceType.CHARIOT else "r"
 
 
 def piece_name(piece_type: PieceType) -> str:
@@ -31,178 +32,69 @@ class Color(Enum):
   BLACK = False
 
 
+# fmt: off
 class Square(Enum):
-  A0 = 0
-  A1 = 1
-  A2 = 2
-  A3 = 3
-  A4 = 4
-  A5 = 5
-  A6 = 6
-  A7 = 7
-  A8 = 8
-  A9 = 9
-  B0 = 10
-  B1 = 11
-  B2 = 12
-  B3 = 13
-  B4 = 14
-  B5 = 15
-  B6 = 16
-  B7 = 17
-  B8 = 18
-  B9 = 19
-  C0 = 20
-  C1 = 21
-  C2 = 22
-  C3 = 23
-  C4 = 24
-  C5 = 25
-  C6 = 26
-  C7 = 27
-  C8 = 28
-  C9 = 29
-  D0 = 30
-  D1 = 31
-  D2 = 32
-  D3 = 33
-  D4 = 34
-  D5 = 35
-  D6 = 36
-  D7 = 37
-  D8 = 38
-  D9 = 39
-  E0 = 40
-  E1 = 41
-  E2 = 42
-  E3 = 43
-  E4 = 44
-  E5 = 45
-  E6 = 46
-  E7 = 47
-  E8 = 48
-  E9 = 49
-  F0 = 50
-  F1 = 51
-  F2 = 52
-  F3 = 53
-  F4 = 54
-  F5 = 55
-  F6 = 56
-  F7 = 57
-  F8 = 58
-  F9 = 59
-  G0 = 60
-  G1 = 61
-  G2 = 62
-  G3 = 63
-  G4 = 64
-  G5 = 65
-  G6 = 66
-  G7 = 67
-  G8 = 68
-  G9 = 69
-  H0 = 70
-  H1 = 71
-  H2 = 72
-  H3 = 73
-  H4 = 74
-  H5 = 75
-  H6 = 76
-  H7 = 77
-  H8 = 78
-  H9 = 79
-  I0 = 80
-  I1 = 81
-  I2 = 82
-  I3 = 83
-  I4 = 84
-  I5 = 85
-  I6 = 86
-  I7 = 87
-  I8 = 88
-  I9 = 89
+  A1 = 0;  A2 = 1;  A3 = 2;  A4 = 3;  A5 = 4;  A6 = 5;  A7 = 6;  A8 = 7;  A9 = 8;  A10 = 9  # noqa: E702
+  B1 = 10; B2 = 11; B3 = 12; B4 = 13; B5 = 14; B6 = 15; B7 = 16; B8 = 17; B9 = 18; B10 = 19 # noqa: E702
+  C1 = 20; C2 = 21; C3 = 22; C4 = 23; C5 = 24; C6 = 25; C7 = 26; C8 = 27; C9 = 28; C10 = 29 # noqa: E702
+  D1 = 30; D2 = 31; D3 = 32; D4 = 33; D5 = 34; D6 = 35; D7 = 36; D8 = 37; D9 = 38; D10 = 39 # noqa: E702
+  E1 = 40; E2 = 41; E3 = 42; E4 = 43; E5 = 44; E6 = 45; E7 = 46; E8 = 47; E9 = 48; E10 = 49 # noqa: E702
+  F1 = 50; F2 = 51; F3 = 52; F4 = 53; F5 = 54; F6 = 55; F7 = 56; F8 = 57; F9 = 58; F10 = 59 # noqa: E702
+  G1 = 60; G2 = 61; G3 = 62; G4 = 63; G5 = 64; G6 = 65; G7 = 66; G8 = 67; G9 = 68; G10 = 69 # noqa: E702
+  H1 = 70; H2 = 71; H3 = 72; H4 = 73; H5 = 74; H6 = 75; H7 = 76; H8 = 77; H9 = 78; H10 = 79 # noqa: E702
+  I1 = 80; I2 = 81; I3 = 82; I4 = 83; I5 = 84; I6 = 85; I7 = 86; I8 = 87; I9 = 88; I10 = 89 # noqa: E702
+# fmt: on
 
 
-# TODO: This need refractor
 STARTING_POSITION = {
-  "RED_SOLDIER": 0b000000000_000000000_000000000_000000000_000000000_000000000_101010101_000000000_000000000_000000000,
-  "RED_CANNON": 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_010000010_000000000_000000000,
-  "RED_GENERAL": 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000010000,
-  "RED_ADVISOR": 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000101000,
-  "RED_ELEPHANT": 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_001000100,
-  "RED_HORSE": 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_010000010,
-  "RED_CHARIOT": 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_100000001,
-  "BLACK_SOLDIER": 0b000000000_000000000_000000000_101010101_000000000_000000000_000000000_000000000_000000000_000000000,
-  "BLACK_CANNON": 0b000000000_000000000_010000010_000000000_000000000_000000000_000000000_000000000_000000000_000000000,
-  "BLACK_GENERAL": 0b000010000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000,
-  "BLACK_ADVISOR": 0b000101000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000,
-  "BLACK_ELEPHANT": 0b001000100_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000,
-  "BLACK_HORSE": 0b010000010_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000,
-  "BLACK_CHARIOT": 0b100000001_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000,
+  Color.RED: {
+    PieceType.SOLDIER: 0b000000000_000000000_000000000_000000000_000000000_000000000_101010101_000000000_000000000_000000000,
+    PieceType.CANNON: 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_010000010_000000000_000000000,
+    PieceType.GENERAL: 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000010000,
+    PieceType.ADVISOR: 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000101000,
+    PieceType.ELEPHANT: 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_001000100,
+    PieceType.HORSE: 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_010000010,
+    PieceType.CHARIOT: 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_100000001,
+  },
+  Color.BLACK: {
+    PieceType.SOLDIER: 0b000000000_000000000_000000000_101010101_000000000_000000000_000000000_000000000_000000000_000000000,
+    PieceType.CANNON: 0b000000000_000000000_010000010_000000000_000000000_000000000_000000000_000000000_000000000_000000000,
+    PieceType.GENERAL: 0b000010000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000,
+    PieceType.ADVISOR: 0b000101000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000,
+    PieceType.ELEPHANT: 0b001000100_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000,
+    PieceType.HORSE: 0b010000010_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000,
+    PieceType.CHARIOT: 0b100000001_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000,
+  },
+  "MISC": {
+    "VERTICAL": 0b100000000_100000000_100000000_100000000_100000000_100000000_100000000_100000000_100000000_100000000,
+    "HORIZONTAL": 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_111111111,
+  },
 }
 
 
+@dataclass
 class Piece:
-  VERTICAL = 0b100000000_100000000_100000000_100000000_100000000_100000000_100000000_100000000_100000000_100000000
-  HORIZONTAL = 0b000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_111111111
-  NAME: str
+  piece_type: PieceType
+  color: Color
+  bit: int = field(init=False)
 
-  def __init__(self, side: Color):
-    piece_name = self.__class__.NAME
-    self.side = side
-    # TODO: This need refractor
-    self.bit = STARTING_POSITION[f"RED_{piece_name}"] if side == Color.RED else STARTING_POSITION[f"BLACK_{piece_name}"]
+  def __post_init__(self):
+    self.bit = STARTING_POSITION[self.color][self.piece_type]
 
   def __repr__(self):
-    return self.NAME[0].upper() if self.side == Color.RED else self.NAME[0].lower()
-
-
-class Soldier(Piece):
-  NAME = "SOLDIER"
-
-
-class Cannon(Piece):
-  NAME = "CANNON"
-
-
-class General(Piece):
-  NAME = "GENERAL"
-
-
-class Advisor(Piece):
-  NAME = "ADVISOR"
-
-
-class Elephant(Piece):
-  NAME = "ELEPHANT"
-
-
-class Horse(Piece):
-  NAME = "HORSE"
-
-
-class Chariot(Piece):
-  NAME = "CHARIOT"
+    s = piece_symbol(self.piece_type)
+    return s.upper() if self.color == Color.RED else s.lower()
 
 
 class Board:
   def __init__(self):
-    piece_types = [Soldier, Cannon, General, Advisor, Elephant, Horse, Chariot]
-    self.red_pieces: list[Piece] = [piece(side=Color.RED) for piece in piece_types]
-    self.black_pieces: list[Piece] = [piece(side=Color.BLACK) for piece in piece_types]
+    self.red_pieces: list[Piece] = [Piece(piece_type=pt, color=Color.RED) for pt in PieceType]
+    self.black_pieces: list[Piece] = [Piece(piece_type=pt, color=Color.BLACK) for pt in PieceType]
     self.all_pieces = self.red_pieces + self.black_pieces
-    self.turn = Color.RED
+    # self.turn = Color.RED
 
-  @staticmethod
-  def parse_move(move: str):
-    r, c = move[0], move[1]
-    r = "ABCDEFGHI".index(r.upper())
-    c = int(c)
-    return r, c
-
-  def at(self, start: tuple[int, int]) -> Optional[Piece]:
-    mask = 1 << (start[0] * FILES + start[1])
+  def at(self, square: Square) -> Optional[Piece]:
+    mask = 1 << square.value
     for piece in self.all_pieces:
       if piece.bit & mask:
         return piece
@@ -211,6 +103,6 @@ class Board:
   def __repr__(self):
     ret = ""
     for i in range(RANKS):
-      row = " ".join(str(s) if (s := self.at(start=(i, j))) is not None else "." for j in range(FILES))
+      row = " ".join(str(s) if (s := self.at(square=Square(i * FILES + j))) is not None else "." for j in range(FILES))
       ret = f"{row}\n{ret}"
     return ret
